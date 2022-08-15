aead7937f2b75395c061767ca439ce658693bb3a
xy <3293232930@qq.com>
2022-08-15 12:20:36 +0800

    add eslint

--- src/components/mixin/model-mixin.ts
+++ src/components/mixin/model-mixin.ts
@@ -1,4 +1,28 @@
+import {computed, defineComponent} from "vue";
 
-export const props = {
-
-}
+export const ModelMixin = defineComponent({
+    props: {
+        /**
+         * 用于双向绑定
+         */
+        modelValue: {
+            required: true
+        }
+    },
+    emits: {
+        /**
+         * v-model change
+         */
+        change: null
+    },
+    computed: {
+        value: {
+            get() {
+                return this.modelValue
+            },
+            set(val: unknown) {
+                this.$emit('change', val)
+            },
+        },
+    }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
+import eslintPlugin from "vite-plugin-eslint";
 function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir);
 }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin({
+    include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+  })],
   resolve: {
     alias: [
       // /@/xxxx => src/xxxx
