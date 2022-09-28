c5378d7dbc97f9cb6f1650f7f56347a857f6eec1
xy <3293232930@qq.com>
2022-09-28 15:09:33 +0800

    组件注册方式调整

--- src/components/ly-component.ts
+++ src/components/ly-component.ts
@@ -4,11 +4,11 @@ import LyForm from '/@/components/form/form/ly-form.vue'
 import LyFormItem from '/@/components/form/form-item/ly-form-item.vue'
 import LyToggle from '/@/components/special/ly-toggle.vue'
 export const LyComponent = {
-  install: (app: import('vue').App) => {
-    app.component('LyIcon', LyIcon)
-    app.component('LyInput', LyInput)
-    app.component('LyForm', LyForm)
-    app.component('LyFormItem', LyFormItem)
-    app.component('LyToggle', LyToggle)
+  install: (Vue: import('vue').App) => {
+    Vue.component('LyIcon', LyIcon)
+    Vue.component('LyInput', LyInput)
+    Vue.component('LyForm', LyForm)
+    Vue.component('LyFormItem', LyFormItem)
+    Vue.component('LyToggle', LyToggle)
   }
 }
